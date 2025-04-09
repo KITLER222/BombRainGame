f0ebff835ad7d60f3def2ff94346f46d41f9be37
Антонина Арабаджи <tonyaarabadzi@mail.ru>
2025-04-09 18:54:11 +0300

    добавлены методы и их описание

--- BombRainGame/Program.cs
+++ BombRainGame/Program.cs
@@ -11,11 +11,16 @@
 
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
 
+            while(lives > 0)
+            {
+
+            }
         }
 
         /// <summary>
-        /// 
+        /// С шансом 1 к 5 добавляет * в случайную врехнюю ячейку поля
         /// </summary>
         static void SpawnBomb()
         {
@@ -27,37 +32,80 @@
         }
 
         /// <summary>
-        /// 
+        /// Перемещает все бомбына скроку ниже (двухмерный массив)
         /// </summary>
         static void MoveBombs()
         {
+            for (int y = height - 2; y >= 0; y--)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (field[x,y] == '*')
+                    {
+                        field[x, y] = ' ';
+                        if(y + 1 < height)
+                        {
+                            field[x, y + 1] = '*';
+                        }
+                    }
+                }
+            }
 
+            for (int x = 0; x < width; x++)
+            {
+                if(field[x, 0] != '*')
+                {
+                    field[x, 0] = ' ';
+                }
+            }
         }
         
         /// <summary>
-        /// 
+        /// Если бомба на позиции игррока - уменьшаем жизнь и убираем бомбу
         /// </summary>
         static void CheckCollision()
         {
-
+            if (field[playerX, height - 1] == '*')
+            {
+                lives--;
+                Console.Beep();
+                field[playerX, height - 1] = ' ';
+            }
         }
 
         /// <summary>
-        /// 
+        /// Выводим текущее состояние поля и статистику
         /// </summary>
         static void DrawField()
         {
-
+            Console.Clear();
+            for (int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    if(y == height - 1 && x == playerX)
+                    {
+                        Console.Write("@");
+                    }
+                    else
+                    {
+                        Console.Write(field[x,y] == '*' ? "*" : ".");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\n Жизни: {lives}");
         }
 
         /// <summary>
-        /// 
+        /// Управление игроком через <- и ->
         /// </summary>
         static void HandleInput()
         {
             if (Console.KeyAvailable)
             {
                 ConsoleKey key = Console.ReadKey(true).Key;
+
                 if (key == ConsoleKey.LeftArrow && playerX > 0)
                 {
                     playerX--;
